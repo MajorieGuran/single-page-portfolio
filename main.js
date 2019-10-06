107b775656110afb408641f0bddcef58e06c057c
MajorieGuran <Majorieguran43@gmail.com>
2019-10-06 12:07:44 +0800

    Updates

---
+++ main.js
@@ -0,0 +1,15 @@
+function appendMailTo(){
+    let message = document.getElementById("mailMessage").value;
+    let subject = document.getElementById("mailSubject").value;
+    console.log(subject);
+    console.log(message);
+
+    let mialToAttr = document.getElementById("sendMail");
+    mailToAttr.setAttribute("href", "mailTo:zollandreaguran43@gmail.com?subject="+subject+"&body="+message);
+
+}
+
+let date = new Date();
+let yearPos = document.getElementById(getYear);
+let currYear = date.getFullYear();
+yearPos.innerHTML = currYear;
